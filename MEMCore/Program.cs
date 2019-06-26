3dd434e423a7bd971262337a9fa03a1fd427cd06
Deepak Shaw <Deepak.Shaw@chisholm.edu.au>
2019-06-26 14:30:52 +1000

    Add project files.

---
+++ MEMCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MEMCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
